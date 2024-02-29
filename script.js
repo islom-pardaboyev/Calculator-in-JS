d603b0e7de27485ab5731d9e9b6f0e9cc6e1f96b
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-02-29 11:20:48 +0500

    Updated File

---
+++ script.js
@@ -0,0 +1,106 @@
+let runningTotal = 0;
+let buffer = '0';
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'C':
+            clear();
+            break;
+        case '=':
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal.toString();
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
+    }
+}
+
+function handleMath(symbol) {
+    if (buffer === '0') {
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    switch (previousOperator) {
+        case '+':
+            runningTotal += intBuffer;
+            break;
+        case '-':
+            runningTotal -= intBuffer;
+            break;
+        case '×':
+            runningTotal *= intBuffer;
+            break;
+        case '÷':
+            if (intBuffer === 0) {
+                buffer = 'Error';
+            } else {
+                runningTotal /= intBuffer;
+            }
+            break;
+    }
+}
+
+function handleNumber(numberString) {
+    if (buffer === '0' || buffer === 'Error') {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+}
+
+function clear() {
+    buffer = '0';
+    runningTotal = 0;
+    previousOperator = null;
+}
+
+function init() {
+    const buttons = document.querySelectorAll('.calc-button');
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            buttonClick(this.innerText);
+        });
+    });
+}
+
+init();
